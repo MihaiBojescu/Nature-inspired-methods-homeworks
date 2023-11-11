abbbf0e72dd77732f437ce02c4bc019f658125ef
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-11-12 01:43:54 +0200

    Homework 1: Use an iterative quicksort algorithm

--- src/util/sort.py
+++ src/util/sort.py
@@ -1,13 +1,31 @@
 import typing as t
 
 
-def quicksort(
-    data: t.List[any], comparator: t.Callable[[any, any], bool]
-) -> t.List[any]:
-    if len(data) <= 1:
-        return data
-    else:
-        pivot = data[0]
-        left = [x for x in data[1:] if comparator(x, pivot)]
-        right = [x for x in data[1:] if not comparator(x, pivot)]
-        return quicksort(left, comparator) + [pivot] + quicksort(right, comparator)
+def quicksort(data: t.List[any], comparator: t.Callable[[any, any], bool]):
+    stack = [(0, len(data) - 1)]
+
+    while stack:
+        low, high = stack.pop()
+
+        if low < high:
+            pivot_index = partition(data, low, high, comparator)
+            stack.append((low, pivot_index - 1))
+            stack.append((pivot_index + 1, high))
+
+    return data
+
+
+def partition(
+    data: t.List[any], low: int, high: int, comparator: t.Callable[[any, any], bool]
+):
+    pivot = data[high]
+    i = low - 1
+
+    for j in range(low, high):
+        if comparator(data[j], pivot):
+            i += 1
+            data[i], data[j] = data[j], data[i]
+
+    data[i + 1], data[high] = data[high], data[i + 1]
+
+    return i + 1
