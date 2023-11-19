8cfdfb06a09e6bd2d2ab9046b0d6a67788a504a2
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-11-19 20:52:46 +0200

    Homework 2: Add fitness to individual

--- src/pso/individual.py
+++ src/pso/individual.py
@@ -6,14 +6,17 @@ import numpy.typing as npt
 class Individual:
     __position: npt.NDArray[np.float32]
     __personal_best_position: npt.NDArray[np.float32]
+    __fitness: np.float32
+    __personal_best_fitness: np.float32
     __velocity: npt.NDArray[np.float32]
     __inertia_bias: np.float32
     __personal_best_position_bias: np.float32
     __team_best_position_bias: np.float32
     __random_jitter_bias: np.float32
-    __personal_best_comparator: t.Callable[
+    __fitness_function: t.Callable[
         [npt.NDArray[np.float32], npt.NDArray[np.float32]], npt.NDArray[np.float32]
     ]
+    __fitness_compare_function: t.Callable[[np.float32, np.float32], bool],
 
     def __init__(
         self,
@@ -22,10 +25,8 @@ class Individual:
         personal_best_position_bias: np.float32,
         team_best_position_bias: np.float32,
         random_jitter_bias: np.float32,
-        personal_best_comparator: t.Callable[
-            [npt.NDArray[np.float32], npt.NDArray[np.float32]], npt.NDArray[np.float32]
-        ] = lambda a, b: a
-        < b,
+        fitness_function: t.Callable[[np.float32], np.float32],
+        fitness_compare_function: t.Callable[[np.float32, np.float32], bool],
     ) -> None:
         self.__position = initial_position
         self.__velocity = np.array(
@@ -36,19 +37,24 @@ class Individual:
         self.__personal_best_position_bias = personal_best_position_bias
         self.__team_best_position_bias = team_best_position_bias
         self.__random_jitter_bias = random_jitter_bias
-        self.__personal_best_comparator = personal_best_comparator
+        self.__fitness_function = fitness_function
+        self.__fitness_compare_function = fitness_compare_function
 
     @property
     def position(self) -> npt.NDArray[np.float32]:
         return self.__position
 
     @property
-    def velocity(self) -> npt.NDArray[np.float32]:
-        return self.__velocity
-
-    @property
     def personal_best_position(self) -> npt.NDArray[np.float32]:
         return self.__personal_best_position
+    
+    @property
+    def fitness(self) -> np.float32:
+        return self.__fitness
+
+    @property
+    def personal_best_fitness(self) -> np.float32:
+        return self.__personal_best_fitness
 
     def update(self, team_best_position: npt.NDArray[np.float32]) -> None:
         self.__velocity = self.__velocity * self.__inertia_bias
@@ -64,8 +70,10 @@ class Individual:
         )
 
         self.__position += self.__velocity
+        self.__fitness = self.__fitness_function(self.__position)
 
-        if self.__personal_best_comparator(
-            self.__position, self.__personal_best_position
+        if self.__fitness_compare_function(
+            self.__fitness, self.__personal_best_fitness
         ):
             self.__personal_best_position = self.__position
+            self.__personal_best_fitness = self.__fitness
