c80b1dc47d0b0b6da95cdc9c095762c537c6df2e
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-11-27 21:58:02 +0200

    Homework 2: Add other optimisation functions

---
+++ src/functions/rastringin.py
@@ -0,0 +1,20 @@
+import typing as t
+import numpy as np
+from functions.definition import FunctionDefinition, FunctionBoundaries
+
+
+def rastrigin(x: t.List[np.float32]):
+    A = 10
+    n = len(x)
+    return A * n + np.sum(
+        [x[i] ** 2 - A * np.cos(2 * np.pi * x[i]) for i in range(len(x))]
+    )
+
+
+rastringin_definition = FunctionDefinition(
+    name="Rastringin",
+    function=rastrigin,
+    target="minimise",
+    value_boundaries=FunctionBoundaries(min=-5.12, max=5.12),
+    best_result=0,
+)
