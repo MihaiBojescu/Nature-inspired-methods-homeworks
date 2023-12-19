019ae54e6f7cbae344a846400ba656970fe88032
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-12-20 00:19:02 +0200

    Homework 2: Initial C-PSO implementation

---
+++ src/algorithms/cpso/algorithm.py
@@ -0,0 +1,162 @@
+import typing as t
+import numpy as np
+import numpy.typing as npt
+from algorithms.cpso.individual import Individual
+from algorithms.base.algorithm import BaseAlgorithm
+from functions.combinatorial.definition import CombinatorialFunctionDefinition
+from util.sort import maximise, minimise, quicksort
+
+T = t.TypeVar("T")
+U = t.TypeVar("U")
+V = t.TypeVar("V")
+
+
+class CombinatorialParticleSwarmOptimisation(BaseAlgorithm):
+    _population: t.List[Individual]
+    _generation: np.uint64
+    _fitness_compare_function: t.Callable[[np.float32, np.float32], bool]
+    _criteria_function: t.Callable[[t.List[np.float32], np.float32, np.uint64], bool]
+
+    __debug: bool
+
+    def __init__(
+        self,
+        generate_initial_population: t.Callable[[], npt.NDArray[np.float32]],
+        fitness_function: t.Callable[[np.float32], np.float32],
+        fitness_compare_function: t.Callable[[np.float32, np.float32], bool],
+        criteria_function: t.Callable[[t.List[U], np.float32, np.uint64], bool],
+        inertia_weight: np.float32,
+        cognitive_parameter: np.float32,
+        social_parameter: np.float32,
+        intensification_parameter: np.float32,
+        debug: bool = False,
+    ) -> None:
+        self._population = [
+            Individual(
+                initial_position=position,
+                inertia_weight=inertia_weight,
+                cognitive_parameter=cognitive_parameter,
+                social_parameter=social_parameter,
+                intensification_parameter=intensification_parameter,
+                fitness_function=fitness_function,
+                fitness_compare_function=fitness_compare_function,
+            )
+            for position in generate_initial_population()
+        ]
+        self._generation = np.uint64(0)
+        self._fitness_compare_function = fitness_compare_function
+        self._criteria_function = criteria_function
+
+        self.__debug = debug
+
+        self._population = quicksort(
+            data=self._population,
+            comparator=lambda a, b: self._fitness_compare_function(
+                a.fitness, b.fitness
+            ),
+        )
+
+    @staticmethod
+    def from_function_definition(
+        function_definition: CombinatorialFunctionDefinition,
+        dimensions: int = 1,
+        population_size: int = 100,
+        generate_initial_population: t.Union[
+            t.Literal["auto"],
+            t.Callable[[], t.List[U]],
+        ] = "auto",
+        criteria_function: t.Union[
+            t.Literal["auto"],
+            t.Callable[[t.List[U], np.float32, np.uint64], bool],
+        ] = "auto",
+        inertia_bias: float = 0.4,
+        personal_best_position_bias: float = 0.7,
+        team_best_position_bias: float = 0.6,
+        intensification_parameter: np.float32 = 0.2,
+        debug: bool = False,
+    ):
+        def default_criteria_function(_values, _fitness, generation):
+            return generation > 100
+
+        def default_generate_initial_population():
+            return [
+                [
+                    np.random.choice(
+                        function_definition.values,
+                        size=len(function_definition.values),
+                        replace=False,
+                    )
+                    for _ in range(dimensions)
+                ]
+                for _ in range(population_size)
+            ]
+
+        generate_initial_population = (
+            generate_initial_population
+            if generate_initial_population != "auto"
+            else default_generate_initial_population
+        )
+        criteria_function = (
+            criteria_function
+            if criteria_function != "auto"
+            else default_criteria_function
+        )
+        fitness_compare_function = (
+            maximise if function_definition.target == "maximise" else minimise
+        )
+
+        return CombinatorialParticleSwarmOptimisation(
+            generate_initial_population=generate_initial_population,
+            fitness_function=function_definition.function,
+            fitness_compare_function=fitness_compare_function,
+            criteria_function=criteria_function,
+            inertia_weight=inertia_bias,
+            cognitive_parameter=personal_best_position_bias,
+            social_parameter=team_best_position_bias,
+            intensification_parameter=intensification_parameter,
+            debug=debug,
+        )
+
+    @property
+    def name(self) -> str:
+        return "C-PSO algorithm"
+
+    def run(self) -> t.Tuple[np.float32, np.float32, np.uint64]:
+        best_individual = self._population[0]
+
+        while not self._criteria_function(
+            best_individual.position, best_individual.fitness, self._generation
+        ):
+            self.step()
+
+        best_individual = self._population[0]
+
+        return best_individual.position, best_individual.fitness, self._generation
+
+    def step(self) -> t.Tuple[np.float32, np.float32, np.uint64]:
+        self._print(self._generation)
+        best_individual = self._population[0]
+
+        for individual in self._population:
+            individual.update(team_best_position=best_individual.personal_best_position)
+
+        self._population = quicksort(
+            data=self._population,
+            comparator=lambda a, b: self._fitness_compare_function(
+                a.fitness, b.fitness
+            ),
+        )
+        best_individual = self._population[0]
+
+        self._generation += 1
+
+        return best_individual.position, best_individual.fitness, self._generation
+
+    def _print(self, generation: int) -> None:
+        if not self.__debug:
+            return
+
+        best_individual = self._population[0]
+        print(
+            f"Combinatorial particle swarm optimisation algorithm generation {generation}: {best_individual.fitness}"
+        )
---
+++ src/algorithms/cpso/individual.py
@@ -0,0 +1,143 @@
+import typing as t
+import numpy as np
+import numpy.typing as npt
+
+
+class Individual:
+    __x: npt.NDArray[np.float32]
+    __personal_best_x: npt.NDArray[np.float32]
+    __fitness: np.float32
+    __personal_best_fitness: np.float32
+    __velocity: npt.NDArray[np.float32]
+    __inertia_weight: np.float32
+    __cognitive_parameter: np.float32
+    __social_parameter: np.float32
+    __intensification_parameter: np.float32
+    __fitness_function: t.Callable[[npt.NDArray[np.float32]], np.float32]
+    __fitness_compare_function: t.Callable[[np.float32, np.float32], bool]
+
+    def __init__(
+        self,
+        initial_position: npt.NDArray[np.float32],
+        inertia_weight: np.float32,
+        cognitive_parameter: np.float32,
+        social_parameter: np.float32,
+        intensification_parameter: np.float32,
+        fitness_function: t.Callable[[npt.NDArray[np.float32]], np.float32],
+        fitness_compare_function: t.Callable[[np.float32, np.float32], bool],
+    ) -> None:
+        self.__x = np.array(initial_position)
+        self.__velocity = np.array(
+            [np.random.uniform(low=-1, high=1) for _ in range(len(self.__x))]
+        )
+        self.__personal_best_x = np.copy(initial_position)
+        self.__inertia_weight = inertia_weight
+        self.__cognitive_parameter = cognitive_parameter
+        self.__social_parameter = social_parameter
+        self.__intensification_parameter = intensification_parameter
+        self.__fitness_function = fitness_function
+        self.__fitness_compare_function = fitness_compare_function
+
+        self.__fitness = self.__fitness_function(self.__x)
+        self.__personal_best_fitness = self.__fitness
+
+    @property
+    def position(self) -> npt.NDArray[np.float32]:
+        return self.__x
+
+    @property
+    def personal_best_position(self) -> npt.NDArray[np.float32]:
+        return self.__personal_best_x
+
+    @property
+    def fitness(self) -> np.float32:
+        return self.__fitness
+
+    @property
+    def personal_best_fitness(self) -> np.float32:
+        return self.__personal_best_fitness
+
+    def update(self, team_best_position: npt.NDArray[np.float32]) -> None:
+        y = self.__calculate_initial_y(team_best_position)
+
+        self.__velocity = self.__velocity * self.__inertia_weight
+        self.__velocity += (
+            (-1 - y) * self.__cognitive_parameter * np.random.uniform(low=-1, high=1)
+        )
+        self.__velocity += (
+            (1 - y) * self.__social_parameter * np.random.uniform(low=-1, high=1)
+        )
+
+        lambda_t = self.__velocity + y
+        y = self.__calculate_y_by_lambda(lambda_t)
+
+        self.__x = self.__calculate_x_by_y(
+            y,
+            team_best_position[i],
+        )
+        self.__fitness = self.__fitness_function(self.__x)
+
+        if self.__fitness_compare_function(
+            self.__fitness, self.__personal_best_fitness
+        ):
+            self.__personal_best_x = self.__x
+            self.__personal_best_fitness = self.__fitness
+
+    def __calculate_initial_y(
+        self, team_best_x: npt.NDArray[np.uint32]
+    ) -> npt.NDArray[np.int8]:
+        y = np.zeros(self.__x.shape, dtype=np.int8)
+
+        for i in range(len(y)):
+            if (
+                self.__x[i] == self.__personal_best_x[i]
+                and self.__x[i] == team_best_x[i]
+            ):
+                y[i] = np.random.choice([-1, 1], size=1)
+                continue
+
+            if self.__x[i] == self.__personal_best_x[i]:
+                y[i] = -1
+                continue
+
+            if self.__x[i] == team_best_x[i]:
+                y[i] = 1
+                continue
+
+        return y
+
+    def __calculate_y_by_lambda(
+        self, lambda_t: npt.NDArray[np.int8]
+    ) -> npt.NDArray[np.int8]:
+        y = np.zeros(self.__x.shape, dtype=np.int8)
+
+        for i in range(len(y)):
+            if lambda_t[i] > self.__intensification_parameter:
+                y[i] = 1
+                continue
+
+            if lambda_t[i] < -self.__intensification_parameter:
+                y[i] = -1
+                continue
+
+        return y
+
+    def __calculate_x_by_y(
+        self,
+        y: npt.NDArray[np.int8],
+        team_best_x: npt.NDArray[np.uint32],
+    ) -> int:
+        x = np.zeros(self.__x.shape)
+
+        for i in range(len(x)):
+            if y[i] == 1:
+                x[i] = team_best_x
+                continue
+
+            if y[i] == -1:
+                x[i] = self.__personal_best_x
+                continue
+
+            x[i] = np.random.randint(low=0, high=len(x))
+
+        return x
