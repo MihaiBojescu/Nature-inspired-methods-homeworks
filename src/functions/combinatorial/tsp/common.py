40d125dff46bb326de51595a3f13c076eecd9b89
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-12-20 02:00:11 +0200

    Homework 2: Add segmentation support for TSP, trim generate initial population, fix distance function

--- src/functions/combinatorial/tsp/common.py
+++ src/functions/combinatorial/tsp/common.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import numpy as np
 import typing as t
 from functions.combinatorial.definition import CombinatorialFunctionDefinition
@@ -19,16 +19,53 @@ class TspResultOptimalMinMax:
 
 @dataclass
 class TspResult:
-    optimal_min_max: TspResultOptimalMinMax = TspResultOptimalMinMax()
+    optimal_min_max: TspResultOptimalMinMax = field(
+        default_factory=TspResultOptimalMinMax
+    )
     optimal_cost: float = 0
+
+
+def segment_instances(
+    fn: t.Callable[[t.List[t.List[City]]], TspResult], dimensions: int
+) -> t.Callable[[t.List[City]], TspResult]:
+    if dimensions < 1:
+        raise IndexError("Dimension < 1 in segment function")
+
+    sections = None
+
+    def run(salesmen_routes: t.List[t.List[City]]) -> TspResult:
+        nonlocal sections
+
+        if sections is None:
+            sections = [0]
+            current_index = 0
+            max_size = (len(salesmen_routes) // 2) - 1
+
+            for _ in range(dimensions - 1):
+                size = np.random.randint(
+                    low=1,
+                    high=max_size,
+                )
+                sections.append(current_index + size)
+                current_index += size
+
+            sections.append(len(salesmen_routes))
+
+        salesmen_routes = [
+            salesmen_routes[sections[i] : sections[i + 1]] for i in range(len(sections) - 1)
+        ]
+        result = fn(salesmen_routes)
+        return result
+
+    return run
 
 
 def augment_instances(
     fn: t.Callable[[t.List[t.List[City]]], TspResult], home_city: City
 ) -> t.Callable[[t.List[t.List[City]]], TspResult]:
     def run(salesmen_routes: t.List[t.List[City]]) -> TspResult:
-        for i in range(salesmen_routes):
-            salesmen_routes[i].insert(0, home_city)
+        for i, _ in enumerate(salesmen_routes):
+            salesmen_routes[i] = np.concatenate(([home_city], salesmen_routes[i]))
 
         result = fn(salesmen_routes)
         return result
@@ -53,50 +90,27 @@ def tsp(salesman_route: t.List[City]) -> float:
     total_cost = 0
 
     for i in range(len(salesman_route) - 1):
-        total_cost += __calculate_distance(salesman_route[i], salesman_route[i] + 1)
+        total_cost += __calculate_distance(salesman_route[i], salesman_route[i + 1])
 
     return total_cost
 
 
 def __calculate_distance(a: City, b: City) -> float:
-    return np.sqrt(((a.x - b.x) ^ 2 + (a.y - b.y) ^ 2))
+    return np.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2)
 
 
 def generate_initial_population(
     function_definition: CombinatorialFunctionDefinition,
     population_size: int,
-    dimensions: int,
 ):
     def run():
-        population = []
-
-        pickable_cities = len(function_definition.values) - 1
-        tour_max_pickable_cities = (pickable_cities // 2) - 1
-        unpicked_cities = np.random.choice(
-            function_definition.values[1:], size=pickable_cities, replace=False
-        )
-
-        for _ in range(population_size):
-            unpicked_cities_copy = unpicked_cities.copy()
-            picked_city_tours = []
-
-            for _ in range(dimensions - 1):
-                picked_tour_size = np.random.randint(
-                    low=1,
-                    high=min(
-                        len(unpicked_cities_copy),
-                        tour_max_pickable_cities,
-                    ),
-                )
-                picked_cities, unpicked_cities_copy = (
-                    unpicked_cities_copy[:picked_tour_size],
-                    unpicked_cities_copy[picked_tour_size:],
-                )
-                picked_city_tours.append(picked_cities)
-
-            picked_city_tours.append(unpicked_cities_copy)
-            population.append(picked_city_tours)
-
-        return population
+        return [
+            np.random.choice(
+                function_definition.values[1:],
+                size=len(function_definition.values) - 1,
+                replace=False,
+            )
+            for _ in range(population_size)
+        ]
 
     return run
