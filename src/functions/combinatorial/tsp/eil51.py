cfca8bd4e225bb45879baacbf1d72ffc08df52cb
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-12-20 02:01:34 +0200

    Homework 2: Update eil51 definition

--- src/functions/combinatorial/tsp/eil51.py
+++ src/functions/combinatorial/tsp/eil51.py
@@ -1,13 +1,23 @@
 from functions.combinatorial.definition import CombinatorialFunctionDefinition
 from functions.combinatorial.tsp.parser import Parser
-from functions.combinatorial.tsp.common import augment_instances, min_max_multiple_tsp
-
-__parser_result = Parser().parse('./src/functions/combinatorial/eil51.tsp')
-
-eil51 = CombinatorialFunctionDefinition(
-    name=__parser_result.name,
-    description=__parser_result.description,
-    function=augment_instances(min_max_multiple_tsp, __parser_result.coordinates[0]),
-    target='minimise',
-    values=__parser_result.coordinates
+from functions.combinatorial.tsp.common import (
+    augment_instances,
+    min_max_multiple_tsp,
+    segment_instances,
 )
+
+__parser_result = Parser().parse("./src/functions/combinatorial/tsp/eil51.tsp")
+
+
+def make_eil51(dimensions: int) -> CombinatorialFunctionDefinition:
+    function = min_max_multiple_tsp
+    function = augment_instances(fn=function, home_city=__parser_result.coordinates[0])
+    function = segment_instances(fn=function, dimensions=dimensions)
+
+    return CombinatorialFunctionDefinition(
+        name=__parser_result.name,
+        description=__parser_result.description,
+        function=function,
+        target="minimise",
+        values=__parser_result.coordinates,
+    )
