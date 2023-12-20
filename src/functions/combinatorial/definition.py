6e970fb4065d190babfa469ede87a0ab1582efe7
Mihai Bojescu <bojescu.mihai@gmail.com>
2023-12-20 02:00:28 +0200

    Homework 2: Remove 1 additional type variable in function definition

--- src/functions/combinatorial/definition.py
+++ src/functions/combinatorial/definition.py
@@ -5,12 +5,11 @@ from dataclasses import dataclass
 
 T = t.TypeVar("T")
 U = t.TypeVar("U")
-V = t.TypeVar("V")
 
 @dataclass
 class CombinatorialFunctionDefinition:
     name: str
     description: str
-    function: t.Callable[[t.List[U]], V]
+    function: t.Callable[[t.List[T]], U]
     target: t.Union[t.Literal["maximise"], t.Literal["minimise"]]
     values: t.List[T]
