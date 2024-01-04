13b2c49e484ae8d2dd2a388aae93e0381f6f12fa
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-01-04 23:44:03 +0200

    Homework 3: Use copy of values in APSO operators

--- src/algorithms/apso/operators.py
+++ src/algorithms/apso/operators.py
@@ -1,6 +1,7 @@
 import typing as t
 import numpy as np
 
+
 class TwoOptOperator:
     __fitness_function: t.Callable[[t.List[np.int64]], np.float32]
     __fitness_compare_function: t.Callable[[np.float32, np.float32], bool]
@@ -14,23 +15,24 @@ class TwoOptOperator:
         self.__fitness_compare_function = fitness_compare_function
 
     def run(self, values: t.List[np.uint64]):
+        values_copy = values.copy()
         improved = True
 
         while improved:
             improved = False
 
-            for i in range(1, len(values) - 2):
-                for j in range(i + 1, len(values) - 1):
-                    a = values[i - 1]
-                    b = values[i]
-                    c = values[j]
-                    d = values[j + 1]
+            for i in range(1, len(values_copy) - 2):
+                for j in range(i + 1, len(values_copy) - 1):
+                    a = values_copy[i - 1]
+                    b = values_copy[i]
+                    c = values_copy[j]
+                    d = values_copy[j + 1]
 
                     if self.__is_cost_lower_after_swap(a, b, c, d):
-                        values[b:d] = reversed(values[b:d])
+                        values_copy[b:d] = reversed(values_copy[b:d])
                         improved = True
 
-        return values
+        return values_copy
 
     def __is_cost_lower_after_swap(self, a: np.int64, b: np.int64, c: np.int64, d: np.int64):
         original_cost = self.__fitness_function([a, b]) + self.__fitness_function([c, d])
@@ -52,20 +54,21 @@ class PathLinkerOperator:
         self.__fitness_compare_function = fitness_compare_function
 
     def run(self, values: t.List[np.int64]):
+        values_copy = values.copy()
         improved = True
 
         while improved:
             improved = False
 
-            for i in range(len(values) - 1):
-                for j in range(i + 1, len(values)):
-                    swapped_values = self.__swap(values, i, j)
+            for i in range(len(values_copy) - 1):
+                for j in range(i + 1, len(values_copy)):
+                    swapped_values = self.__swap(values_copy, i, j)
 
-                    if self.__compare(values, swapped_values):
-                        values = swapped_values
+                    if self.__compare(values_copy, swapped_values):
+                        values_copy = swapped_values
                         improved = True
 
-        return values
+        return values_copy
 
     def __swap(self, values: t.List[np.int64], i: int, j: int):
         swapped_values = values.copy()
@@ -79,9 +82,12 @@ class PathLinkerOperator:
 
         return self.__fitness_compare_function(original_cost, swapped_cost)
 
+
 class SwapOperator:
     def run(self, values: t.List[np.int64]):
-        [a, b] = np.random.choice(list(range(len(values))))
-        values[a], values[b] = values[b], values[a]
+        values_copy = values.copy()
 
-        return values
+        [a, b] = np.random.choice(list(range(len(values_copy))), size=2, replace=False)
+        values_copy[a], values_copy[b] = values_copy[b], values_copy[a]
+
+        return values_copy
