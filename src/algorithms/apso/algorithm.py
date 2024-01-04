e557a526ca32b2eac2d25e5807fa689f0200abbf
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-01-04 23:44:52 +0200

    Homework 3: Algorithm and metered algorithm

---
+++ src/algorithms/apso/algorithm.py
@@ -0,0 +1,127 @@
+import typing as t
+import numpy as np
+import numpy.typing as npt
+from algorithms.apso.individual import Individual
+from algorithms.base.algorithm import BaseAlgorithm
+from functions.combinatorial.definition import CombinatorialFunctionDefinition
+from util.sort import maximise, minimise, quicksort
+
+
+class AdaptiveParticleSwarmOptimisation(BaseAlgorithm):
+    _population: t.List[Individual]
+    _generation: np.uint64
+    _fitness_compare_function: t.Callable[[np.float32, np.float32], bool]
+    _criteria_function: t.Callable[[t.List[np.int64], np.float32, np.uint64], bool]
+
+    __debug: bool
+
+    def __init__(
+        self,
+        generate_initial_population: t.Callable[[], npt.NDArray[np.float32]],
+        fitness_function: t.Callable[[t.List[np.int64]], np.float32],
+        fitness_compare_function: t.Callable[[np.float32, np.float32], bool],
+        criteria_function: t.Callable[[t.List[np.int64], np.float32, np.uint64], bool],
+        debug: bool = False,
+    ) -> None:
+        self._population = [
+            Individual(
+                initial_position=position,
+                fitness_function=fitness_function,
+                fitness_compare_function=fitness_compare_function,
+            )
+            for position in generate_initial_population()
+        ]
+        self._generation = np.uint64(0)
+        self._fitness_compare_function = fitness_compare_function
+        self._criteria_function = criteria_function
+
+        self.__debug = debug
+
+        self._population = quicksort(
+            data=self._population,
+            comparator=lambda a, b: self._fitness_compare_function(a.fitness, b.fitness),
+        )
+
+    @staticmethod
+    def from_function_definition(
+        function_definition: CombinatorialFunctionDefinition,
+        dimensions: int = 1,
+        population_size: int = 100,
+        generate_initial_population: t.Union[
+            t.Literal["auto"],
+            t.Callable[[], t.List[np.int64]],
+        ] = "auto",
+        criteria_function: t.Union[
+            t.Literal["auto"],
+            t.Callable[[t.List[np.int64], np.float32, np.uint64], bool],
+        ] = "auto",
+        debug: bool = False,
+    ):
+        def default_criteria_function(_values, _fitness, generation):
+            return generation > 100
+
+        def default_generate_initial_population():
+            return [
+                [
+                    np.random.choice(
+                        function_definition.values,
+                        size=len(function_definition.values),
+                        replace=False,
+                    )
+                    for _ in range(dimensions)
+                ]
+                for _ in range(population_size)
+            ]
+
+        generate_initial_population = (
+            generate_initial_population
+            if generate_initial_population != "auto"
+            else default_generate_initial_population
+        )
+        criteria_function = criteria_function if criteria_function != "auto" else default_criteria_function
+        fitness_compare_function = maximise if function_definition.target == "maximise" else minimise
+
+        return AdaptiveParticleSwarmOptimisation(
+            generate_initial_population=generate_initial_population,
+            fitness_function=function_definition.function,
+            fitness_compare_function=fitness_compare_function,
+            criteria_function=criteria_function,
+            debug=debug,
+        )
+
+    @property
+    def name(self) -> str:
+        return "APSO algorithm"
+
+    def run(self) -> t.Tuple[np.float32, np.float32, np.uint64]:
+        best_individual = self._population[0]
+
+        while not self._criteria_function(best_individual.position, best_individual.fitness, self._generation):
+            self.step()
+
+        best_individual = self._population[0]
+
+        return best_individual.position, best_individual.fitness, self._generation
+
+    def step(self) -> t.Tuple[t.List[np.int64], np.float32, np.uint64]:
+        self._print(self._generation)
+
+        for individual in self._population:
+            individual.update()
+
+        self._population = quicksort(
+            data=self._population,
+            comparator=lambda a, b: self._fitness_compare_function(a.fitness, b.fitness),
+        )
+        self._generation += 1
+
+        best_individual = self._population[0]
+
+        return best_individual.position, best_individual.fitness, self._generation
+
+    def _print(self, generation: int) -> None:
+        if not self.__debug:
+            return
+
+        best_individual = self._population[0]
+        print(f"Adaptive particle swarm optimisation algorithm generation {generation}: {best_individual.fitness}")
---
+++ src/algorithms/apso/metered_algorithm.py
@@ -0,0 +1,110 @@
+import time
+import typing as t
+import numpy as np
+import numpy.typing as npt
+from algorithms.apso.algorithm import AdaptiveParticleSwarmOptimisation
+from functions.combinatorial.definition import CombinatorialFunctionDefinition
+from util.sort import maximise, minimise
+
+
+class MeteredAdaptiveParticleSwarmOptimisation(AdaptiveParticleSwarmOptimisation):
+    __metrics_runtime: t.List[t.Tuple[np.uint64, np.uint64]]
+    __metrics_values: t.List[t.Tuple[np.uint64, t.List[np.int64]]]
+    __metrics_fitness: t.List[t.Tuple[np.uint64, np.float32]]
+
+    def __init__(
+        self,
+        generate_initial_population: t.Callable[[], npt.NDArray[np.float32]],
+        fitness_function: t.Callable[[np.float32], np.float32],
+        fitness_compare_function: t.Callable[[np.float32, np.float32], bool],
+        criteria_function: t.Callable[[t.List[np.int64], np.float32, np.uint64], bool],
+        debug: bool = False,
+    ) -> None:
+        super().__init__(
+            generate_initial_population=generate_initial_population,
+            fitness_function=fitness_function,
+            fitness_compare_function=fitness_compare_function,
+            criteria_function=criteria_function,
+            debug=debug,
+        )
+
+        self.__metrics_runtime = []
+        self.__metrics_values = []
+        self.__metrics_fitness = []
+
+    @staticmethod
+    def from_function_definition(
+        function_definition: CombinatorialFunctionDefinition,
+        dimensions: int = 1,
+        population_size: int = 100,
+        generate_initial_population: t.Union[
+            t.Literal["auto"],
+            t.Callable[[], t.List[np.int64]],
+        ] = "auto",
+        criteria_function: t.Union[
+            t.Literal["auto"],
+            t.Callable[[t.List[np.int64], np.float32, np.uint64], bool],
+        ] = "auto",
+        debug: bool = False,
+    ):
+        def default_criteria_function(_values, _fitness, generation):
+            return generation > 100
+
+        def default_generate_initial_population():
+            return [
+                [
+                    np.random.choice(
+                        function_definition.values,
+                        size=len(function_definition.values),
+                        replace=False,
+                    )
+                    for _ in range(dimensions)
+                ]
+                for _ in range(population_size)
+            ]
+
+        generate_initial_population = (
+            generate_initial_population
+            if generate_initial_population != "auto"
+            else default_generate_initial_population
+        )
+        criteria_function = criteria_function if criteria_function != "auto" else default_criteria_function
+        fitness_compare_function = maximise if function_definition.target == "maximise" else minimise
+
+        return MeteredAdaptiveParticleSwarmOptimisation(
+            generate_initial_population=generate_initial_population,
+            fitness_function=function_definition.function,
+            fitness_compare_function=fitness_compare_function,
+            criteria_function=criteria_function,
+            debug=debug,
+        )
+
+    def run(self) -> t.Tuple[t.List[np.int64], np.float32, np.uint64]:
+        then = time.time_ns()
+        best_individual = self._population[0]
+
+        while not self._criteria_function(best_individual.position, best_individual.fitness, self._generation):
+            self.step()
+
+            now = time.time_ns()
+            self.__metrics_runtime.append((self._generation, now - then))
+
+            for individual in self._population:
+                self.__metrics_values.append((self._generation, individual.position))
+                self.__metrics_fitness.append((self._generation, individual.fitness))
+
+        best_individual = self._population[0]
+
+        return best_individual.position, best_individual.fitness, self._generation
+
+    @property
+    def metrics_runtime(self) -> t.List[t.Tuple[np.uint64, np.uint64]]:
+        return self.__metrics_runtime
+
+    @property
+    def metrics_values(self) -> t.List[t.Tuple[np.uint64, t.List[np.int64]]]:
+        return self.__metrics_values
+
+    @property
+    def metrics_fitness(self) -> t.List[t.Tuple[np.uint64, np.float32]]:
+        return self.__metrics_fitness
